8828d0b0f7fe96e4046740aaf9559fa9ae128f65
camillelancon <camille.lancon@student-cs.fr>
2023-11-10 15:32:26 +0000

    Add notification when a terminal is created or killed

--- helloworld/out/extension.js
+++ helloworld/out/extension.js
@@ -35,16 +35,38 @@ function showNotification(message) {
 function activate(context) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "helloworld" is now active!');
+    console.log('Congratulations, your extension "helloworld" is active!');
+    let terminals = [];
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
-    const terminalListener = vscode.window.onDidOpenTerminal((terminal) => {
-        const message = `New terminal created: ${terminal.name}`;
+    const openTerminalListener = vscode.window.onDidOpenTerminal((terminal) => {
+        const message = `Remember to run the command "script" in the new terminal`;
         showNotification(message);
+        terminals.push(terminal);
     });
-    // Make sure to dispose of the event listener when the extension is deactivated
-    context.subscriptions.push(terminalListener);
+    // Register the terminal close event listener
+    const closeTerminalDisposable = vscode.window.onDidCloseTerminal((closedTerminal) => {
+        // Register a command to handle terminal closure
+        const terminal = vscode.window.activeTerminal;
+        if (terminal && terminals.includes(terminal)) {
+            vscode.window.showInformationMessage(`Did you remember to run the command "exit" in the terminal? `, { modal: true }, 'Yes').then((choice) => {
+                if (choice === 'Yes') {
+                    // User confirmed, close the terminal
+                    terminal.dispose();
+                    const closeMessage = `Terminal closed`;
+                    showNotification(closeMessage);
+                    // Remove the closed terminal from the array
+                    terminals = terminals.filter((t) => t !== closedTerminal);
+                }
+                else {
+                    // User canceled, reopen the terminal
+                    vscode.window.createTerminal().show();
+                }
+            });
+        }
+    });
+    context.subscriptions.push(openTerminalListener, closeTerminalDisposable);
 }
 exports.activate = activate;
 // This method is called when your extension is deactivated
--- helloworld/src/extension.ts
+++ helloworld/src/extension.ts
@@ -10,26 +10,47 @@ function showNotification(message: string): void {
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "helloworld" is active!');
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "helloworld" is active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const openTerminalListener = vscode.window.onDidOpenTerminal((terminal) => {
-        const message = `New terminal: ${terminal.name}`;
-        showNotification(message);
-	});
+    let terminals: vscode.Terminal[] = [];
 
-	const closeTerminalListener = vscode.window.onDidCloseTerminal((terminal) => {
-        const message = `Terminal closed: ${terminal.name}`;
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with registerCommand
+    // The commandId parameter must match the command field in package.json
+    const openTerminalListener = vscode.window.onDidOpenTerminal((terminal) => {
+        const message = `Remember to run the command "script" in the new terminal`;
         showNotification(message);
-	});
-	
-	// Make sure to dispose of the event listener when the extension is deactivated
-    context.subscriptions.push(openTerminalListener,closeTerminalListener);
+        terminals.push(terminal);
+
+    });
+
+    // Register the terminal close event listener
+    const closeTerminalDisposable = vscode.window.onDidCloseTerminal( (closedTerminal) => {
+        // Register a command to handle terminal closure
+            const terminal = vscode.window.activeTerminal;
+            if (terminal && terminals.includes(terminal)) {
+                vscode.window.showInformationMessage(`Did you remember to run the command "exit" in the terminal? `, { modal: true }, 'Yes').then((choice) => {
+                    if (choice === 'Yes') {
+                        // User confirmed, close the terminal
+                        terminal.dispose();
+                        const closeMessage = `Terminal closed`;
+                        showNotification(closeMessage);
+
+                        // Remove the closed terminal from the array
+                        terminals = terminals.filter((t) => t !== closedTerminal);
+                    } else {
+                        // User canceled, reopen the terminal
+                        vscode.window.createTerminal().show();
+                    }
+                });
+            }   
+    });
+
+    context.subscriptions.push(openTerminalListener,closeTerminalDisposable);
 }
+
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
