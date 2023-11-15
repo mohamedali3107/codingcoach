853e8eda6151b86a7fa90bd09f96ad591e628d3f
me <me@mes-MacBook-Air.local>
2023-11-15 13:47:58 +0100

    Reads repo

--- gitAPI/read.py
+++ gitAPI/read.py
@@ -1,1 +1,8 @@
 import gitlab
+
+gl = gitlab.Gitlab(url = 'https://gitlab.paris-digital-lab.com', private_token='glpat-qsH-B46AAzU8vCDw1yV3')
+
+project = gl.projects.get('centralesupelec-fall2023-p2/codingcoach')
+items = project.repository_tree()
+
+print(items)
