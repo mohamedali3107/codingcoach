eb6f43f4c995e667cc6d18c6d61551f7fe2b3291
me <me@mes-MacBook-Air.local>
2023-11-16 12:20:09 +0100

    Warning messages

--- gitAPI/read.py
+++ gitAPI/read.py
@@ -4,6 +4,9 @@ gl = gitlab.Gitlab(url = 'https://gitlab.paris-digital-lab.com', private_token='
 project = gl.projects.get('centralesupelec-fall2023-p2/codingcoach')
 
 def compare_branches(project):
+    '''
+    Takes a project and returns a list of branch names and numbers of commits ahead and behind.
+    '''
     branches = project.branches.list()
     L = []
     for branch in branches:
@@ -13,5 +16,16 @@ def compare_branches(project):
             L.append((branch.name, ahead_by, behind_by))
     return L
 
-for name, ahead_by, behind_by in compare_branches(project):
-    print(f"{name} is ahead by {ahead_by} commits and behind by {behind_by} commits compared to main.")
+def warning_message(L):
+    s = ''
+    for name, ahead_by, behind_by in L:
+        if ahead_by >= 10:
+            s += f'{name} is ahead of main by {ahead_by} commits, consider merging\n'
+        if behind_by >= 10:
+            s += f'{name} is behind main by {behind_by} commits, consider merging\n'''
+    return s
+
+print(warning_message(compare_branches(project)))
+
+#for name, ahead_by, behind_by in compare_branches(project):
+#    print(f"{name} is ahead by {ahead_by} commits and behind by {behind_by} commits compared to main.")
