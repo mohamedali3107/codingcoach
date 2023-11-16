eab571ddc37a9cba7d7905a818f08feae7331c5f
me <me@mes-MacBook-Air.local>
2023-11-16 11:44:58 +0100

    Corrected ahead and behind

--- gitAPI/read.py
+++ gitAPI/read.py
@@ -8,6 +8,6 @@ branches = project.branches.list()
 
 for branch in branches:
     if branch.name != 'main':
-        ahead_by = len(project.repository_compare(branch.name, 'main')['commits'])
-        behind_by = len(project.repository_compare('main', branch.name)['commits'])
+        behind_by = len(project.repository_compare(branch.name, 'main')['commits'])
+        ahead_by = len(project.repository_compare('main', branch.name)['commits'])
         print(f"Branch '{branch.name}' is ahead by {ahead_by} commits and behind by {behind_by} commits compared to 'main'.")
