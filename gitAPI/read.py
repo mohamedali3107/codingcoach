2f8d2e0b163f2cca34843ab2e1d25822bea4fe4e
me <me@mes-MacBook-Air.local>
2023-11-16 11:40:44 +0100

    Shows branches ahead and behind

--- gitAPI/read.py
+++ gitAPI/read.py
@@ -4,5 +4,10 @@ gl = gitlab.Gitlab(url = 'https://gitlab.paris-digital-lab.com', private_token='
 
 project = gl.projects.get('centralesupelec-fall2023-p2/codingcoach')
 items = project.repository_tree()
+branches = project.branches.list()
 
-print(items)
+for branch in branches:
+    if branch.name != 'main':
+        ahead_by = len(project.repository_compare(branch.name, 'main')['commits'])
+        behind_by = len(project.repository_compare('main', branch.name)['commits'])
+        print(f"Branch '{branch.name}' is ahead by {ahead_by} commits and behind by {behind_by} commits compared to 'main'.")
