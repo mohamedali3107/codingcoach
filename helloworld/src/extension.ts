341c4c947a655fb1709c39dfa42f80441140f6de
camillelancon <camille.lancon@student-cs.fr>
2023-11-16 15:40:21 +0000

    Retrieve and process data from git status command

--- helloworld/src/extension.ts
+++ helloworld/src/extension.ts
@@ -1,44 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import * as fs from 'fs';
-
-
-
-function showNotification(message: string): void {
-    vscode.window.showInformationMessage(message);
-}
-
-
-export async function executeGitCommandAndGetOutput(command: string, terminal: vscode.Terminal, duration: number): Promise<string> {
-    
-    // Check if there's an active workspace folder
-    const activeWorkspace = vscode.workspace.workspaceFolders?.[0];
-
-    if (!activeWorkspace) {
-        throw new Error('No active workspace found.');
-    }
-
-    // Generate a temporary file path within the workspace folder to store the command output
-    const tempFilePath = vscode.Uri.joinPath(activeWorkspace.uri, 'git_output_temp.txt').fsPath;
-
-    // Run the Git command in the terminal
-    terminal.sendText(`${command} > ${tempFilePath}`);
-
-    // Wait for some time to collect the output (adjust this time according to your command's execution time)
-    await new Promise(resolve => setTimeout(resolve, duration)); // Wait for 2 seconds (adjust as needed)
-
-   // Read the contents of the temporary file (command output)
-   const output = fs.readFileSync(tempFilePath, 'utf-8');
-
-   // Remove the temporary file
-   fs.unlinkSync(tempFilePath);
-
-   // Return the captured output
-   return output;
-}
-
-
+import { showNotification, executeGitCommandAndGetOutput, gitStatusCommand } from "./tools/git-notification";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -56,10 +19,15 @@ export function activate(context: vscode.ExtensionContext) {
         showNotification(message);
         terminals.push(terminal);
 
-
         // Execute a Git command in the terminal and retrieve the output
-        executeGitCommandAndGetOutput("git status", terminal, 2000)
-            .then((value) => {(console.log("Out:", value))})
+        executeGitCommandAndGetOutput("git status", terminal, 5000)
+            .then((value) =>
+                {
+                    for (const line of gitStatusCommand(value)) {
+                        console.log(line)
+                    }
+                }
+            )
             .catch((e) => {
                 console.log('Erreur', e)
         })
@@ -89,7 +57,6 @@ export function activate(context: vscode.ExtensionContext) {
 
     context.subscriptions.push(openTerminalListener,closeTerminalDisposable);
 }
-
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
