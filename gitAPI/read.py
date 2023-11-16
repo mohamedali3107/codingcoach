b5a1c3da46e57472f18d4a16075c10f2e959e029
me <me@mes-MacBook-Air.local>
2023-11-16 11:57:31 +0100

    Removed useless line

--- gitAPI/read.py
+++ gitAPI/read.py
@@ -3,11 +3,10 @@ import gitlab
 gl = gitlab.Gitlab(url = 'https://gitlab.paris-digital-lab.com', private_token='glpat-qsH-B46AAzU8vCDw1yV3')
 
 project = gl.projects.get('centralesupelec-fall2023-p2/codingcoach')
-items = project.repository_tree()
 branches = project.branches.list()
 
 for branch in branches:
     if branch.name != 'main':
         behind_by = len(project.repository_compare(branch.name, 'main')['commits'])
         ahead_by = len(project.repository_compare('main', branch.name)['commits'])
-        print(f"Branch '{branch.name}' is ahead by {ahead_by} commits and behind by {behind_by} commits compared to 'main'.")
+        print(f"{branch.name} is ahead by {ahead_by} commits and behind by {behind_by} commits compared to main.")
