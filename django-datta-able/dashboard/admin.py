28224ee31f922f1e0f7188a01d1c131d3e505946
camillelancon <camille.lancon@student-cs.fr>
2023-11-20 13:59:28 +0000

    Merge with main

--- django-datta-able/dashboard/admin.py
+++ django-datta-able/dashboard/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import CoachTable, Utilisateur, TeamTable, TeamMood, TeamRepo
 
 # Register your models here.
+
+@admin.register(CoachTable)
+class CoachModelAdmin(admin.ModelAdmin):
+    list_display = ('username',)  # Customize the fields to display
+
+@admin.register(Utilisateur)
+class UtilisateurModelAdmin(admin.ModelAdmin):
+    list_display = ('username',)  # Customize the fields to display
+
+@admin.register(TeamTable)
+class TeamTableModelAdmin(admin.ModelAdmin):
+    list_display = ('teamId',)  # Customize the fields to display
+
+@admin.register(TeamMood)
+class TeamModelAdmin(admin.ModelAdmin):
+    list_display = ('timeStamp',)  # Customize the fields to display
+
+@admin.register(TeamRepo)
+class TeamRepoModelAdmin(admin.ModelAdmin):
+    list_display = ('timeStamp',)  # Customize the fields to display
--- django-datta-able/dashboard/urls.py
+++ django-datta-able/dashboard/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.contrib import admin
 
 from . import views
 app_name = 'dashboard'
@@ -17,6 +18,7 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('admin/', admin.site.urls)
 
     # Ajoutez d'autres URLs au besoin
 ]
