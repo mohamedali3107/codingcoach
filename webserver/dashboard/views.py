b3782a63df0d79acc9c1ccf9665030d4d81d0fe6
thomasStbr <thomas.stauber@student-cs.fr>
2023-11-22 14:10:04 +0000

    login pages

--- webserver/dashboard/views.py
+++ webserver/dashboard/views.py
@@ -1,14 +1,30 @@
 from django.http import HttpRequest
-from .forms import RegisterForm
+from .forms import RegisterForm, TeamTableForm
 from django.shortcuts import redirect, render
 from django.contrib.auth import login , logout , authenticate 
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required , user_passes_test
 # Create your views here.
 
-@login_required(login_url="/login")
+@user_passes_test(lambda u: u.is_authenticated or u.is_superuser, login_url="/login")
 def home(request : HttpRequest):
     print(request.user.id )
     return render(request , 'dashboard/index.html')
+
+@login_required(login_url="/login")
+def addTeam(request):
+    if request.method == 'POST':
+        form = TeamTableForm(request.POST)
+        if form.is_valid():
+            team = form.save(commit=False)
+            # Set additional fields if needed
+            team.save()
+            return redirect('dashboard:home')  # Redirect to the appropriate page after saving
+    else:
+        # Set the initial data for the form
+        initial_data = {'coachs': [request.user.id]}
+        form = TeamTableForm(initial=initial_data)
+
+    return render(request, 'dashboard/add_team.html', {'form': form})
 
 
 def sign_up(request):
@@ -23,3 +39,16 @@ def sign_up(request):
         form = RegisterForm()
     
     return render(request , "registration/signup.html" , {"form" :  form })
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def create_user(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('user_list')  # Redirect to the user list page or any other page
+    else:
+        form = RegisterForm()
+
+    return render(request, 'registration/create_user.html', {'form': form})
---
+++ webserver/utils/gitAPI.py
@@ -0,0 +1,90 @@
+import gitlab
+'''
+If you encounter library compatibilty problems when installing python-gitlab, try running
+pip3 uninstall urllib3
+pip3 install 'urllib3<2.0'
+'''
+gl = gitlab.Gitlab(url = 'https://gitlab-cw1.centralesupelec.fr', private_token='glpat-fULiHV8-x78CbwdNsz6w')
+project = gl.projects.get('amin.belfkira/game2048')
+def get_most_ahead(project):
+    branches = project.branches.list()
+    max_ahead = 0
+    for branch in branches:
+        if branch.name != 'main':
+            ahead_by = len(project.repository_compare('main', branch.name)['commits'])
+            if ahead_by > max_ahead:
+                max_ahead = ahead_by
+                most_ahead = branch.name
+    return most_ahead
+
+
+def get_most_behind(project):
+    branches = project.branches.list()
+    max_behind = 0
+    for branch in branches:
+        if branch.name != 'main':
+            behind_by = len(project.repository_compare(branch.name, 'main')['commits'])
+            if behind_by > max_behind:
+                max_behind = behind_by
+                most_behind = branch.name
+    return most_behind
+
+
+def get_branch_number(project):
+    return len(project.branches.list())
+
+
+def get_last_PR(project): # untested because nobody does PRs
+    merge_requests = project.mergerequests.list()
+    if len(merge_requests) == 0:
+        return None
+    merge_requests.sort(key=lambda x: x.created_at, reverse=True)
+    return merge_requests[0]
+
+
+def compare_branches(project):
+    '''
+    Takes a project and returns a list of branch names and numbers of commits ahead and behind.
+    '''
+    branches = project.branches.list()
+    L = []
+    for branch in branches:
+        if branch.name != 'main':
+            behind_by = len(project.repository_compare(branch.name, 'main')['commits'])
+            ahead_by = len(project.repository_compare('main', branch.name)['commits'])
+            L.append((branch.name, ahead_by, behind_by))
+    return L
+def rate_commits(project):
+    malus = 0
+    for commit in project.commits.list(get_all=False):
+        size = 0
+        for diff in commit.diff(get_all=False):
+            lines = diff['diff'].split('\n')
+            for line in lines:
+                if line != '' and (line[0] == '-' or line[0] == '+'):
+                    size += 1
+        if size > 40:
+            print(commit.message)
+            malus += min((size - 30)/30, 1)
+    return 20 - malus
+
+def warning_message(L):
+    s = ''
+    for branch, ahead_by, behind_by in L:
+        if ahead_by >= 10:
+            s += f'''{branch} is ahead of main by {ahead_by} commits, consider merging:
+    git checkout main
+    git pull
+    git merge {branch}
+'''
+        if behind_by >= 10:
+            s += f'''{branch} is behind main by {behind_by} commits, consider rebasing:
+    git checkout main
+    git pull
+    git checkout {branch}
+    git rebase main
+'''
+    return s
+#print(warning_message(compare_branches(project)))
+#for name, ahead_by, behind_by in compare_branches(project):
+#    print(f"{name} is ahead by {ahead_by} commits and behind by {behind_by} commits compared to main.")
