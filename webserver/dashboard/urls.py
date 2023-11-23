ea09e6c2a69462fd5cf2029411b9dda8232b8ba2
thomasStbr <thomas.stauber@student-cs.fr>
2023-11-23 13:24:38 +0000

    included mohammed ali

--- webserver/dashboard/urls.py
+++ webserver/dashboard/urls.py
@@ -6,9 +6,9 @@ app_name = "dashboard"
 
 urlpatterns = [
     path('' , views.home , name='home'),
-    path('signup' , views.sign_up , name="signup"),
-    path('addTeam' , views.addTeam , name="addTeam"), 
-    path('createUser' ,views.create_user , name="createUser" ),
+    path('signup/' , views.sign_up , name="signup"),
+    path('addTeam/' , views.addTeam , name="addTeam"), 
+    path('createUser/' ,views.create_user , name="createUser" ),
     path('sendMood/', views.MoodView.as_view(), name="Mood")
 
 ]
