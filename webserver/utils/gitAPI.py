64a9cd381466bcc28dd7347deab8a8af8803a026
thomasStbr <thomas.stauber@student-cs.fr>
2023-11-23 12:26:42 +0000

    gitAPI

--- webserver/utils/gitAPI.py
+++ webserver/utils/gitAPI.py
@@ -1,12 +1,30 @@
 import gitlab
+
 '''
 If you encounter library compatibilty problems when installing python-gitlab, try running
 pip3 uninstall urllib3
 pip3 install 'urllib3<2.0'
 '''
+
 gl = gitlab.Gitlab(url = 'https://gitlab-cw1.centralesupelec.fr', private_token='glpat-fULiHV8-x78CbwdNsz6w')
 project = gl.projects.get('amin.belfkira/game2048')
-def get_most_ahead(project):
+
+def get_project(server, token, project):
+    gl = gitlab.Gitlab(url=server, private_token=token)
+    return gl.projects.get(project)
+
+def list_projects_users(server, token):
+    projects = gitlab.Gitlab(url = server, private_token=token).projects.list(get_all=True)
+    L = []
+    for p in projects:
+        path = p.path_with_namespace
+        users = [user.name for user in p.users.list()]
+        L.append([path] + users)
+    return L
+
+
+def most_ahead_branch(server, token, project):
+    project = get_project(server, token, project)
     branches = project.branches.list()
     max_ahead = 0
     for branch in branches:
@@ -17,8 +35,8 @@ def get_most_ahead(project):
                 most_ahead = branch.name
     return most_ahead
 
-
-def get_most_behind(project):
+def most_behind_branch(server, token, project):
+    project = get_project(server, token, project)
     branches = project.branches.list()
     max_behind = 0
     for branch in branches:
@@ -29,23 +47,23 @@ def get_most_behind(project):
                 most_behind = branch.name
     return most_behind
 
-
-def get_branch_number(project):
+def get_branch_number(server, token, project):
+    project = get_project(server, token, project)
     return len(project.branches.list())
 
-
-def get_last_PR(project): # untested because nobody does PRs
+def last_PR_time(server, token, project): # untested because nobody does PRs
+    project = get_project(server, token, project)
     merge_requests = project.mergerequests.list()
     if len(merge_requests) == 0:
         return None
     merge_requests.sort(key=lambda x: x.created_at, reverse=True)
-    return merge_requests[0]
+    return merge_requests[0].created_at
 
-
-def compare_branches(project):
+def compare_branches(server, token, project):
     '''
     Takes a project and returns a list of branch names and numbers of commits ahead and behind.
     '''
+    project = get_project(server, token, project)
     branches = project.branches.list()
     L = []
     for branch in branches:
@@ -54,7 +72,9 @@ def compare_branches(project):
             ahead_by = len(project.repository_compare('main', branch.name)['commits'])
             L.append((branch.name, ahead_by, behind_by))
     return L
-def rate_commits(project):
+
+def rate_commits(server, token, project):
+    project = get_project(server, token, project)
     malus = 0
     for commit in project.commits.list(get_all=False):
         size = 0
@@ -64,27 +84,5 @@ def rate_commits(project):
                 if line != '' and (line[0] == '-' or line[0] == '+'):
                     size += 1
         if size > 40:
-            print(commit.message)
             malus += min((size - 30)/30, 1)
     return 20 - malus
-
-def warning_message(L):
-    s = ''
-    for branch, ahead_by, behind_by in L:
-        if ahead_by >= 10:
-            s += f'''{branch} is ahead of main by {ahead_by} commits, consider merging:
-    git checkout main
-    git pull
-    git merge {branch}
-'''
-        if behind_by >= 10:
-            s += f'''{branch} is behind main by {behind_by} commits, consider rebasing:
-    git checkout main
-    git pull
-    git checkout {branch}
-    git rebase main
-'''
-    return s
-#print(warning_message(compare_branches(project)))
-#for name, ahead_by, behind_by in compare_branches(project):
-#    print(f"{name} is ahead by {ahead_by} commits and behind by {behind_by} commits compared to main.")
