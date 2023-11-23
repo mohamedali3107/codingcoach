e4bc4fd2789db60a0838f15d8804d388f710d718
thomasStbr <thomas.stauber@student-cs.fr>
2023-11-23 12:23:09 +0000

    working server

--- webserver/dashboard/admin.py
+++ webserver/dashboard/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
+from .models import TeamMood, TeamRepo, TeamTable, Utilisateur
+from .models import Coach
+
 # Register your models here.
+@admin.register(Coach)
+class CoachModelAdmin(admin.ModelAdmin):
+    list_display = ('username',)  # Customize the fields to display
+
+@admin.register(Utilisateur)
+class UtilisateurModelAdmin(admin.ModelAdmin):
+    list_display = ('username',)  # Customize the fields to display
+
+@admin.register(TeamTable)
+class TeamTableModelAdmin(admin.ModelAdmin):
+    list_display = ('teamId',)  # Customize the fields to display
+
+@admin.register(TeamMood)
+class TeamModelAdmin(admin.ModelAdmin):
+    list_display = ('timeStamp',)  # Customize the fields to display
+
+@admin.register(TeamRepo)
+class TeamRepoModelAdmin(admin.ModelAdmin):
+    list_display = ('timeStamp',)  # Customize the fields to display
--- webserver/dashboard/views.py
+++ webserver/dashboard/views.py
@@ -1,9 +1,16 @@
+import datetime
 from django.http import HttpRequest
+from .models import TeamMood, Utilisateur
+
+from.serializers import MoodSerializer
 from .forms import RegisterForm, TeamTableForm
 from django.shortcuts import redirect, render
 from django.contrib.auth import login , logout , authenticate 
 from django.contrib.auth.decorators import login_required , user_passes_test
 # Create your views here.
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 @user_passes_test(lambda u: u.is_authenticated or u.is_superuser, login_url="/login")
 def home(request : HttpRequest):
@@ -52,3 +59,30 @@ def create_user(request):
         form = RegisterForm()
 
     return render(request, 'registration/create_user.html', {'form': form})
+
+
+class MoodView(APIView):
+    def post(self, request, format=None):
+        serializer = MoodSerializer(data=request.data)
+
+        if serializer.is_valid():
+            # Valid data
+            moodLevel = serializer.validated_data['moodLevel']
+            message = serializer.validated_data['message']
+            email = serializer.validated_data['email']
+
+            user = Utilisateur.objects.filter(email=email).first()
+
+            team = user.teams.all()
+
+            mood = TeamMood.objects.create(timeStamp=datetime.now(), moodLevel=moodLevel, message=message)
+            mood.teams.set(team)
+
+            return Response({'status': 'success',
+                            'message': 'Data received and processed successfully'},
+                            status=status.HTTP_201_CREATED)
+        else:
+            # Invalid data
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
