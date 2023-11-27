2a3f3351254867f5748a0f8a3c92f7ab524a993c
bobleponge <thomas.stauber@student-cs.fr>
2023-11-27 16:57:43 +0000

    working retrivial

--- webserver/dashboard/admin.py
+++ webserver/dashboard/admin.py
@@ -1,25 +1,28 @@
 from django.contrib import admin
-
-from .models import TeamMood, TeamRepo, TeamTable, Utilisateur
-from .models import Coach
+from .models import TeamMood, TeamRepo, TeamTable, Utilisateur, Coach, GitlabAccessRepo
 
 # Register your models here.
+
 @admin.register(Coach)
 class CoachModelAdmin(admin.ModelAdmin):
-    list_display = ('username',)  # Customize the fields to display
+    list_display = ('username',)
 
 @admin.register(Utilisateur)
 class UtilisateurModelAdmin(admin.ModelAdmin):
-    list_display = ('username',)  # Customize the fields to display
+    list_display = ('username', 'email')
+
+@admin.register(GitlabAccessRepo)
+class GitlabAccessRepoModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'token', 'url', 'projectName')  # Add fields to customize the display
 
 @admin.register(TeamTable)
 class TeamTableModelAdmin(admin.ModelAdmin):
-    list_display = ('teamId',)  # Customize the fields to display
+    list_display = ('teamId', 'teamName')
 
 @admin.register(TeamMood)
-class TeamModelAdmin(admin.ModelAdmin):
-    list_display = ('timeStamp',)  # Customize the fields to display
+class TeamMoodModelAdmin(admin.ModelAdmin):
+    list_display = ('timeStamp', 'moodLevel', 'message')
 
 @admin.register(TeamRepo)
 class TeamRepoModelAdmin(admin.ModelAdmin):
-    list_display = ('timeStamp',)  # Customize the fields to display
+    list_display = ('timeStamp', 'branchNumber', 'branchBehindMax', 'branchAheadMax', 'lastPrTime', 'commitQuality')
