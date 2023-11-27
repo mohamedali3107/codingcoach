08bba84dfcf612ff6ce3a7c2c942552f2db58e70
bobleponge <thomas.stauber@student-cs.fr>
2023-11-27 14:22:43 +0000

    epuration

--- django-datta-able/dashboard/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import TeamMood
-
-class MoodSerializer(serializers.Serializer):
-    moodLevel = serializers.IntegerField()
-    message = serializers.CharField(allow_blank=True)
-    email = serializers.CharField(allow_blank=True)
-
