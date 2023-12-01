77783d501373cd2c2fd07c8f412b602dabf6049e
thomasStbr <thomas.stauber@student-cs.fr>
2023-12-01 13:10:37 +0000

    fix size of username

--- webserver/dashboard/models.py
+++ webserver/dashboard/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User, BaseUserManager
 class Utilisateur(models.Model):
     id = models.AutoField(primary_key=True)
     #email = models.EmailField(unique=True , bla)
-    username = models.CharField(max_length=30, unique=True)
+    username = models.CharField(max_length=250, unique=True)
 
     def __str__(self):
         return self.username
