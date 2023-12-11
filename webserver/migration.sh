423a5518ee0b34a4ec00cbc6f3e56ae415d95801
thomasStbr <thomas.stauber@student-cs.fr>
2023-12-11 09:01:30 +0000

    superuser

--- webserver/migration.sh
+++ webserver/migration.sh
@@ -8,3 +8,10 @@ python3 manage.py makemigrations llmcoach
 
 
 python3 manage.py migrate 
+
+
+export DJANGO_SUPERUSER_USERNAME=root
+export DJANGO_SUPERUSER_EMAIL=myadmin@example.com
+export DJANGO_SUPERUSER_PASSWORD=root
+
+python3 manage.py createsuperuser --noinput
