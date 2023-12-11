3a3ba32d7e57eb34e02aa6a62d653713e6618327
camillelancon <camille.lancon@student-cs.fr>
2023-12-11 10:27:18 +0000

    Clean code

--- student-mood/src/tools/request.ts
+++ student-mood/src/tools/request.ts
@@ -2,18 +2,11 @@ import * as vscode from 'vscode';
 import axios from 'axios';
 
 export async function makePostRequest(values: any) {
-    const url = 'http://localhost:8000/send_mood/'; // Server's URL
+    const url = 'https://coding-assistant.centralesupelec.fr/send_mood/'; // Server's URL
 
     try {
         const response = await axios.post(url, values);
 
-        // Handle the response
-        console.log('Response:', response.data);
-        console.log(response.request)
-        console.log(response.config.url)
-
-        // Show a notification with the response data
-        vscode.window.showInformationMessage('POST request successful!');
     } catch (error) {
         // Handle errors
         console.error('Error:', error);
